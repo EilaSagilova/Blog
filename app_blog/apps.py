b80c5292950bfe5c1649a5bf3a7968e981d81dec
EilaSagilova <example@example.com>
2024-06-03 12:06:51 +0500

    Lab 10

--- app_blog/apps.py
+++ app_blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppBlogConfig(AppConfig):
+class EnBlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_blog"
